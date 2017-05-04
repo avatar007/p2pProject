1ee38869eb11ccc56ec661aa9ed1deccf28c5cb6
木水 <avatar_007@qq.com>
2017-05-04 22:26:12 +0800

    第一次提交

---
+++ P2P项目/src/main/java/com/studio/p2pproject/adapter/MyBaseAdapter.java
@@ -0,0 +1,55 @@
+package com.studio.p2pproject.adapter;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import java.util.List;
+
+/**
+ * 作者: 某某某 于 2017/5/3 11:04
+ * 概述:
+ */
+public abstract class MyBaseAdapter<T> extends BaseAdapter {
+    private List<T> data;
+
+    public MyBaseAdapter(List<T> data) {
+        this.data = data;
+    }
+
+    @Override
+    public int getCount() {
+        return data.size();
+    }
+
+    @Override
+    public T getItem(int position) {
+        return data.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return super.getViewTypeCount();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return super.getItemViewType(position);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup viewGroup) {
+        MyViewHolder holder = null;
+        if (convertView == null){
+            holder = getHolder();
+        }
+        return null;
+    }
+
+    public abstract MyViewHolder getHolder();
+}
